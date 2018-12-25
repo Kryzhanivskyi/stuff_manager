1dfa04474bdb577aeda415ab7ce2a1f325845d6d
Kostiantyn <kostiantyn.kryzhanivskyi@gmail.com>
2018-12-25 18:50:54 +0200

    added urls

--- src/apps/account/views.py
+++ src/apps/account/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from .models import User
 
-def index(request):
-    return HttpResponse("Index")
+
+def profile(request, id):
+    return HttpResponse(User.objects.filter(id=id))
